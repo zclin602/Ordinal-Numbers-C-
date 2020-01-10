2e7219d916f82b2f66434f828e1a6618bf6b95bd
zclin602 <clinard@student.pcsstn.net>
2020-01-10 14:18:05 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,16 @@
+using System;
+
+class MainClass {
+  public static void Main (string[] args) {
+    int maxNumber;
+    int minNumber = 1;
+    Console.WriteLine ("Max number???\n ");
+    Console.ReadLine();
+    if (maxNumber > 100) 
+    {
+      Console.WriteLine("STOP.");
+    }
+
+    Console.ReadLine();
+  }
+}
