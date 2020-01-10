52d606074da570bb32ab09e16275b0e5916e21b1
zclin602 <clinard@student.pcsstn.net>
2020-01-10 15:29:30 +0000

    Added code

--- main.cs
+++ main.cs
@@ -2,14 +2,54 @@ using System;
 
 class MainClass {
   public static void Main (string[] args) {
-    int maxNumber;
-    int minNumber = 1;
-    Console.WriteLine ("Max number???\n ");
-    Console.ReadLine();
-    if (maxNumber > 100) 
-    {
-      Console.WriteLine("STOP.");
+    Console.Write("Max: ");
+    string input = Console.ReadLine();
+
+    try
+     {
+      uint max = UInt32.Parse(input);
+
+      for (int i = 1; i <= max; i++)
+      {
+        if (i <= 0) 
+        {
+          Console.WriteLine(i);
+          continue;
+
+        }
+        switch (i % 100) 
+        {
+          case 11:
+          case 12:
+          case 13:
+           Console.WriteLine($"{i}th");
+           continue;
+
+        }
+        switch(i % 10) 
+         {
+          case 1:
+          Console.WriteLine($"{i}st");
+           continue;
+          case 2:
+          Console.WriteLine($"{i}nd");
+           continue;
+          case 3:
+          Console.WriteLine($"{i}rd");
+           continue;
+           default:
+          Console.WriteLine($"{i}th");
+           continue;
+          
+         }
+
+       }
     }
+    catch(OverflowException) 
+     {
+      Console.WriteLine($"Hey, pal... {input} is out of range of UInt32 you freAKING MORON!!!!!!!");
+
+     }
 
     Console.ReadLine();
   }
